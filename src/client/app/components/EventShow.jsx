3c5116482dbab66940198aeeaf7bfc612ccc2580
Christine Dey <christinedey5@gmail.com>
2017-03-31 10:50:53 -0700

    Edit delete button

--- src/client/app/components/EventShow.jsx
+++ src/client/app/components/EventShow.jsx
@@ -4,19 +4,21 @@ import AttendingList from './AttendingList.jsx';
 import MessageInputBox from './MessageInputBox.jsx';
 import Chatbox from './Chatbox.jsx';
 import CreateEventButton from '../CreateEventButton.jsx';
+import DeleteButton from './DeleteButton.jsx';
 
 const EventShow = (props) => {
   // functions here
   return (
     <div className='event-show'>
       <CreateEventButton friends={ props.friends } getEvents={ props.getEvents } history={ props.history } />
-      <ShortInfo event={ props.event }/>
+      <ShortInfo event={ props.event } />
       <AttendingList
         friends={ props.friends }
         isGoing={ props.isGoing }
         handleDeclineEvent={ props.handleDeclineEvent }
         handleAcceptEvent={ props.handleAcceptEvent }
       />
+      <DeleteButton event={ props.event } />
       <Chatbox
         messages={ props.messages }
         renderNewMessage={ props.renderNewMessage }
